4cc6226d01f205b375a6dc1b8cd2569b62e92102
MHelmiAzhar <helmiazhar311201@gmail.com>
2025-10-05 21:13:21 +0700

    feat: init forntend

---
+++ frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router'
+import router from './router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </QueryClientProvider>
+)
