0a7766a9983815ddebf9fb4771f078072c1ffa04
MHelmiAzhar <helmiazhar311201@gmail.com>
2025-10-06 13:32:26 +0700

    feat: landing page and sign up

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,5 +1,10 @@
 import { createBrowserRouter } from 'react-router'
+import LandingPage from './features/landing/pages/LandingPage'
+import SignUpPage from './features/auth/pages/SignUpPage'
 
-const router = createBrowserRouter([{ path: '/', element: <div>Home</div> }])
+const router = createBrowserRouter([
+  { path: '/', element: <LandingPage /> },
+  { path: 'sign-up', element: <SignUpPage /> }
+])
 
 export default router
